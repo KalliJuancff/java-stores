48c74fd9ecc981dcc645fc810fb2388af17c8cbe
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-12-16 17:34:52 +0100

    feat: update 'create_a_new_store' system test to assert against JSON contents, and update controller to call use case with real request

--- src/main/java/controller/StoresPutController.java
+++ src/main/java/controller/StoresPutController.java
@@ -1,12 +1,11 @@
 package controller;
 
 import application.StoreSaver;
+import com.google.gson.Gson;
 import domain.StoreSaverRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/stores")
@@ -19,12 +18,8 @@ public class StoresPutController {
 
     @PutMapping("/{code}")
     public ResponseEntity<String> saveStore(@PathVariable int code, @RequestBody String body) {
-        StoreSaverRequest request = new StoreSaverRequest(
-                7777,
-                "Store 7777",
-                "",
-                "",
-                UUID.randomUUID().toString());
+        Gson gson = new Gson();
+        StoreSaverRequest request = gson.fromJson(body, StoreSaverRequest.class);
         storeSaver.save(request);
 
         return new ResponseEntity<>("Store saved with code " + code, HttpStatus.CREATED);
--- src/test/java/system/StoresPutControllerFeature.java
+++ src/test/java/system/StoresPutControllerFeature.java
@@ -1,12 +1,20 @@
 package system;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import domain.Store;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,14 +31,14 @@ public class StoresPutControllerFeature {
     }
 
     @Test
-    public void create_a_new_store() {
+    public void create_a_new_store() throws IOException {
         String requestBody = """
                 {
                     "storeCode": "4321",
                     "storeName": "Store #4321",
-                    "storeOpeningDate": "",
-                    "storeClosingDate": "",
-                    "storeExpectedOpeningDate": "2025/2026"
+                    "storeOpeningDate": "2003/12/23",
+                    "storeClosingDate": "2003/12/25",
+                    "storeExpectedOpeningDate": ""
                 }
                 """;
 
@@ -42,6 +50,33 @@ public class StoresPutControllerFeature {
         .then()
                 .statusCode(201);
 
+        ensureThatStoreWasPersistedWithValues("4321", "Store #4321", "2003-12-23", "2003-12-25", "");
+    }
+
+    private void ensureThatStoreWasPersistedWithValues(
+            String code,
+            String name,
+            String openingDate,
+            String closingDate,
+            String expectedOpeningDate
+    ) throws IOException {
         assertThat(Files.exists(Path.of(STORE_JSON))).isTrue();
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) ->
+                        new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE)))
+                .registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, typeOfT, context) ->
+                        LocalDate.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE))
+                .create();
+        var jsonString = Files.readString(Paths.get(STORE_JSON));
+        Type type = new TypeToken<List<Store>>() { }.getType();
+        List<Store> stores = gson.fromJson(jsonString, type);
+        var store = stores.get(0);
+
+        assertThat(store.code()).isEqualTo(Integer.parseInt(code));
+        assertThat(store.name()).isEqualTo(name);
+        assertThat(store.openingDate().get()).isEqualTo(LocalDate.parse(openingDate));
+        assertThat(store.closingDate().get()).isEqualTo(LocalDate.parse(closingDate));
+        assertThat(store.expectedOpeningDate()).isEqualTo(expectedOpeningDate);
     }
 }
