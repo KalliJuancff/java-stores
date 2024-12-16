ecb51b03f86e8d41bf862f317e725ddb18728055
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-12-16 18:20:15 +0100

    fix: decorate controller with '@CrossOrigin' to avoid an CORS error from React sample application

--- src/main/java/controller/StoresPutController.java
+++ src/main/java/controller/StoresPutController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 @RequestMapping("/stores")
 public class StoresPutController {
